c5849b96ae0028fa3802f60a92671ab34faa0d27
Deepak Goyal <88363801+deepak29-git@users.noreply.github.com>
2021-12-09 14:11:23 +0530

    update

--- src/App.js
+++ src/App.js
@@ -10,15 +10,21 @@ var musicGenres = {
     {
       name: "Agnee",
       rating: "4.7/5"
+    },
+    {
+      name: "Pehli Nazar Mein",
+      rating: "3.2/5"
     }
   ],
   Pop: [
     { name: "A Girl Like You", rating: "4.5/5" },
-    { name: "Aaj Phir Jeene Ki", rating: "4.8/5" }
+    { name: "Aaj Phir Jeene Ki", rating: "4.8/5" },
+    { name: "O Sanam", rating: "5/5" }
   ],
   Instrumental: [
     { name: "Tere Bina Zindagi Se", rating: "4/5" },
-    { name: "Pal Pal Dil ke Pas", rating: "4.5/5" }
+    { name: "Pal Pal Dil ke Pas", rating: "4.5/5" },
+    { name: "Kya Yehi Pyaar Hai", rating: "4.2/5" }
   ]
 };
 var musicList = Object.keys(musicGenres);
@@ -35,17 +41,17 @@ export default function App() {
       <h1>Good music</h1>
       <p>Check My Favorite Music</p>
 
-      {musicList.map((list) => (
-        <button key={list} onClick={() => clickGenre(list)}>
+      {musicList.map((list, index) => (
+        <button key={index} onClick={() => clickGenre(list)}>
           {list}
         </button>
       ))}
-      {musicGenres[genre].map((item) => {
+      {musicGenres[genre].map((item, index) => {
         return (
-          <>
+          <div key={index}>
             <h3 className="name">{item.name}</h3>
             <small className="rating">{item.rating}</small>
-          </>
+          </div>
         );
       })}
     </div>
